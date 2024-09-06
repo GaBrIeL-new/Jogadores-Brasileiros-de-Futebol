76ab27b1924ca7de26f42028e0d772a83bcdf1e3
Gabriel Porto <estudosgabriel07@gmail.com>
2024-09-06 19:24:03 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos.
+    let section = document.getElementById("resultados-pesquisa");
+    console.log(section); // Linha para depuração, pode ser removida em produção.
+
+    let campoPesquisa = document.getElementById
+    ("campo-pesquisa").value
+    // se o campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+       section.innerHTML = "<p>nenhum resultado encontrado. você precisa digitar o nome de um jogador brasileiro</p>"
+        return
+    }
+     campoPesquisa = campoPesquisa.toLowerCase()
+    // Inicializa uma string vazia para armazenar os resultados formatados em HTML.
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada dado na lista de dados (assumindo que 'dados' é um array).
+    for (let dado of dados) {
+         titulo = dado.titulo.toLowerCase()
+         descricao = dado.descricao.toLowerCase()
+         tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // Cria um novo elemento 
+            resultados += `<div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">${dado.titulo} </a>
+            </h2>
+            <p class="descrição-meta">${dado.descricao}
+                </p>
+            <a href=${dado.link} target="_blank">mais informações</a>
+        </div>
+        `;
+        }
+        
+    }
+
+   if (!resultados) {
+       resultados = "<p> nenhum resultado encontrado</p>"
+   }
+
+    // Atribui o conteúdo HTML gerado à seção de resultados.
+    section.innerHTML = resultados;
+} 
---
+++ dados.js
@@ -0,0 +1,29 @@
+let dados =[
+
+    { // Objeto para Neymar (já existente)
+     
+    titulo: "Neymar",
+    descricao: "Neymar da Silva Santos Júnior, mais conhecido como Neymar Jr É um dos maiores jogadores de futebol da atualidade.Com uma habilidade impressionante dribles desconcertantes e uma visão de jogo aguçada o brasileiro encantou milhões de fãs ao redor do mundo",
+    link: "https://pt.wikipedia.org/wiki/Neymar",
+    tags: "dribles drible ouro olimpiada lesão "
+    
+  } ,
+  
+  // Objeto para Ronaldo Fenômeno
+   {
+    titulo: "Ronaldo Fenômeno",
+    descricao: "Ronaldo Luís Nazário de Lima, mais conhecido como Ronaldo Fenômeno, é considerado um dos maiores atacantes da história do futebol. Conhecido por sua velocidade, habilidade e faro de gol, conquistou diversos títulos e encantou torcedores ao redor do mundo.",
+    link: "https://pt.wikipedia.org/wiki/Ronaldo_Fenômeno",
+    tags: "fenômeno fenomeno copa do mundo aposentado"
+    
+  } ,
+  
+  // Objeto para Kaká
+   {
+    titulo: "Kaká",
+    descricao: "Ricardo Izecson dos Santos Leite, mais conhecido como Kaká, é um ex-futebolista brasileiro que atuava como meio-campista. Conquistou a Bola de Ouro e o prêmio FIFA de melhor jogador do mundo em 2007, além de ter sido campeão da Copa do Mundo de 2002 com a Seleção Brasileira.",
+    link: "https://pt.wikipedia.org/wiki/Kaká",
+    tags: "perfeito bola de ouro aposentado "
+
+  }
+];
